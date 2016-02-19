bb6ab95b5524d40fa69c50043360bb26a1020563
emmettgarber <emmett.garber@gmail.com>
2016-02-19 05:31:46 +0000

    Add GPS 2.2 file

--- week-5/test.rb
+++ week-5/test.rb
@@ -1,1 +1,18 @@
+def array(text)
+  text_split = text.split
+  list = Hash.new
+  text_split.each do |x|
+    list[x] = 1
+  end
+  list["boy"] = 2
+end
 
+def remove(a)
+  list = Hash.new
+  list = array()
+  list.delete(a)
+  p list
+end
+
+array("The boy went to the store")
+remove("the")
