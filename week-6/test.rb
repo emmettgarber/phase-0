e72cd4403cc889e9f8d7498dc6f3c5c6461561bd
emmettgarber <emmett.garber@gmail.com>
2016-02-25 20:52:50 +0000

    Test file

--- week-6/test.rb
+++ week-6/test.rb
@@ -1,7 +1,15 @@
 def cc(number)
-  array = (number.to_s).split
-
-  p array  
+  array = number.to_s.chars
+  sum_array = Array.new
+  split_array = Array.new
+  while array.count > 0
+    x = array.pop
+    y = array.pop
+    sum_array << (x.to_i)
+    sum_array << (y.to_i * 2)
+  end
+  p sum_array  
+  p split_array
 end
 
 cc(1234523648971212)
