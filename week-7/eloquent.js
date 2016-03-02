798e428cc3561aca9853ab5ffbf2a86a95c47bdf
emmettgarber <emmett.garber@gmail.com>
2016-03-02 16:11:26 +0000

    Add file for 7.2

--- week-7/eloquent.js
+++ week-7/eloquent.js
@@ -4,21 +4,43 @@
 
 // Program Structure
 // Write your own variable and do something to it.
-var test = 10;
-console.log(test + 10);
+// var test = 10;
+// console.log(test + 10);
 
-
+// var food = prompt("Whats your favorite food?", "");
+// alert("That's my favorite food too!");
 
 
 // Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+// var number = 1
+// while (number <= 100) {
+//   if ( (number % 3) == 0 && (number % 5) == 0) console.log("FizzBuzz");
+//   else if ( (number % 3) == 0 ) console.log("Fizz");
+//   else if ((number % 5) == 0) console.log("Buzz");
+//   else console.log(number)
+//   number += 1
+// }
+  
 
 
 
 // Functions
 
 // Complete the `minimum` exercise.
+function min(num1, num2) {
+ console.log(Math.min(num1, num2))
+}
+min(15, 23)
 
 
 
 // Data Structures: Objects and Arrays
 // Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+var me = {
+  name: "Emmett Garber",
+  age: 27,
+  favorite_foods: ["curry", "tapas", "reuben"],
+   quirk: "Eat leftover over dinner for breakfast not breakfast food",
+}
+
+console.log(me)
