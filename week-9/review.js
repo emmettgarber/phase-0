116a9164f2a1adbb33bf633c8f127591dab5e60f
emmettgarber <emmett.garber@gmail.com>
2016-03-15 22:03:48 +0000

    Add files for Jquery task

---
+++ week-9/review.js
@@ -0,0 +1,16 @@
+// psuedocode:
+
+var board = [[47, 44, 71, 8, 88],
+        [22, 69, 75, 65, 73],
+        [83, 85, 97, 89, 57],
+        [25, 31, 96, 68, 51],
+        [75, 70, 54, 80, 83]]
+        
+var letters =["B", "I", "N", "G", "O"]
+
+function bingo() {
+  var callLetter = letters[]
+  // for (var i = 0; i < board.length; i++) {
+  //   if() {}
+  // }
+}
